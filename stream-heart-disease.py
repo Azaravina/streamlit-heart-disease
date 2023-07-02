62d93de29666e4da401be4a6056d00e1f2d0566a
Azaravina <129273438+Azaravina@users.noreply.github.com>
2023-07-02 08:43:59 +0700

    Add files via upload

---
+++ stream-heart-disease.py
@@ -0,0 +1,35 @@
+import pickle
+import streamlit as st
+
+#Load model
+heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
+
+#Judul Web
+st.title('Data Mining Prediksi penyakit Jantung')
+
+
+#Edith Colomn
+col1, col2 = st.columns(2)
+
+with col1 :
+    age                     = st.text_input ('Input data Age')
+
+with col2 :
+    BP                      = st.text_input ('Input data Blood Pressure')
+
+with col1 :
+    cholestrol              = st.text_input ('Input data Cholestrol')
+
+#For Prediction
+heart_diagnosis = ''
+
+#Button
+if st.button('Prediksi Heart Disease'):
+    heart_prediction = heart_disease_model.predict([[age, BP, cholestrol]])
+
+    if(heart_prediction[0] == 1):
+        heart_diagnosis = 'Pasien terkena penyakit Jantung'
+    else :
+        heart_diagnosis = 'Pasien tidak mempunyai penyakit Jantung'
+      
+    st.success(heart_diagnosis)
